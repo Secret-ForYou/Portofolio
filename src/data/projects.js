54feba2857aa0a9357a4bc2413a866c26122f5f6
Secret-ForYou <unbanwa103@gmail.com>
2024-11-14 01:49:42 -0800

    Add files via upload

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    type: 'web',
+    name: 'Happy Fathers Day',
+    description: 'Happy Fathers Day for your dad',
+    src: 'happyfathersday.webp',
+    repository: 'github.com/Secret-ForYou/Happy-Fathers-Day-',
+    site: '#',
+    technologies: ['HTML', 'CSS', 'Js'],
+  },
+
+];
+
+export default projects;
