1e71209910775df6e1659a0c52a20e11265e6f28
Secret-ForYou <unbanwa103@gmail.com>
2024-11-14 02:24:05 -0800

    Delete src directory

--- src/data/contacts.js
+++
@@ -1,6 +0,0 @@
-const contacts = [
-  {social: 'email', href: 'mailto:12301230da@gmail.com', text:'12301230da@gmail.com'},
-  {social: 'github', href: 'https://github.com/Secret-ForYou', text: "/Secret-ForYou"}
-];
-
-export default contacts;
